fc69d4ad32bcac9f5dbc993898f07499c7f2a4b9
Levi Indrajaya Sutantio <alevindrajaya@gmail.com>
2022-06-18 12:47:39 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const binaryInput = document.querySelector(".binary-input");
+const btnConvert = document.querySelector(".btn-convert");
+const resultEl = document.querySelector(".result");
+
+const isBinary = function () {
+    if (binaryInput.value.length > 8) {
+        return false;
+    }
+
+    for (const binaryValue of binaryInput.value) {
+        const binary = Number.parseInt(binaryValue);
+        if (Number.isNaN(binary) || binary > 1) {
+            return false;
+        }
+    }
+    return true;
+};
+
+btnConvert.addEventListener("click", () => {
+    if (!isBinary()) {
+        resultEl.textContent = "Invalid Input";
+        return;
+    }
+
+    let result = 0;
+    for (let i = 0; i < binaryInput.value.length; i++) {
+        const binary = Number.parseInt(binaryInput.value[i]);
+        result += binary * 2 ** (7 - i);
+    }
+    resultEl.textContent = result;
+});
