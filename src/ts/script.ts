90e1564bdec2a977ed168ab9656328b2a361fdb1
Levi Indrajaya Sutantio <alevindrajaya@gmail.com>
2022-06-29 13:57:51 +0700

    fix bug and add typescript

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,20 +1,22 @@
-const binaryInput = document.querySelector(".binary-input");
-const btnConvert = document.querySelector(".btn-convert");
-const resultEl = document.querySelector(".result");
+const binaryInput = document.querySelector(
+    ".binary-input"
+)! as HTMLInputElement;
+const btnConvert = document.querySelector(".btn-convert")!;
+const resultEl = document.querySelector(".result")!;
 
-const isBinary = function () {
+function isBinary() {
     if (binaryInput.value.length > 8) {
         return false;
     }
 
     for (const binaryValue of binaryInput.value) {
-        const binary = Number.parseInt(binaryValue);
+        const binary = parseFloat(binaryValue);
         if (Number.isNaN(binary) || binary > 1) {
             return false;
         }
     }
     return true;
-};
+}
 
 btnConvert.addEventListener("click", () => {
     if (!isBinary()) {
@@ -24,8 +26,8 @@ btnConvert.addEventListener("click", () => {
 
     let result = 0;
     for (let i = 0; i < binaryInput.value.length; i++) {
-        const binary = Number.parseInt(binaryInput.value[i]);
-        result += binary * 2 ** (7 - i);
+        const binary = parseFloat(binaryInput.value[i]);
+        result += binary * 2 ** (binaryInput.value.length - i - 1);
     }
     resultEl.textContent = `the result is: ${result}`;
 });
