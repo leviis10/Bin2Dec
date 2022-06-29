c557815852bf3c88a77baae55816206c85fc897d
Levi Indrajaya Sutantio <alevindrajaya@gmail.com>
2022-06-29 14:01:40 +0700

    add dist folder

---
+++ dist/js/script.js
@@ -0,0 +1,28 @@
+const binaryInput = document.querySelector(".binary-input");
+const btnConvert = document.querySelector(".btn-convert");
+const resultEl = document.querySelector(".result");
+function isBinary() {
+    if (binaryInput.value.length > 8) {
+        return false;
+    }
+    for (const binaryValue of binaryInput.value) {
+        const binary = parseFloat(binaryValue);
+        if (Number.isNaN(binary) || binary > 1) {
+            return false;
+        }
+    }
+    return true;
+}
+btnConvert.addEventListener("click", () => {
+    if (!isBinary()) {
+        resultEl.textContent = "Invalid Input";
+        return;
+    }
+    let result = 0;
+    for (let i = 0; i < binaryInput.value.length; i++) {
+        const binary = parseFloat(binaryInput.value[i]);
+        result += binary * 2 ** (binaryInput.value.length - i - 1);
+    }
+    resultEl.textContent = `the result is: ${result}`;
+});
+//# sourceMappingURL=script.js.map
